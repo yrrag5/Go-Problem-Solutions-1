08bd4710dea3751790afea389f579ecdab61b1bb
College <G00335806@gmit.ie>
2017-09-28 14:30:50 +0100

    Starting repo with solution 1 and readme.md

---
+++ README.md
@@ -0,0 +1,22 @@
+# Problem sheet 1
+My explantions to my code for problem sheet 1
+
+Q1:
+
+Q2:
+
+Q3:
+
+Q4:
+
+Q5:
+
+Q6:
+
+Q7:
+
+Q8:
+
+Q9:
+
+Q10:
---
+++ src/Q1.go
@@ -0,0 +1,13 @@
+
+// Author: Garry Cummins
+// Date: 23-09-2017
+
+// Adapted from : https://tour.golang.org/welcome/1
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("世界!") // Displays the message Hello World! in Japanese 
+}
